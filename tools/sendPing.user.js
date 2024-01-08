d4d42c026a58debb3f05dc4ad3924f36a9432441
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-01-08 09:13:33 +0100

    Create sendPing.user.js

---
+++ tools/sendPing.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        DEBUG: send ping
+// @description script which sends ping message to DataBridge
+// @namespace   Violentmonkey Scripts
+// @match       https://example.org/
+// @grant       GM_info
+// @version     1.0
+// @author      black-backdoor (https://github.com/black-backdoor)
+// @require     https://github.com/black-backdoor/DataBridge/raw/main/DataBridge.lib.user.js
+// ==/UserScript==
+
+// create connection
+const globalConnection = new Connection("LOCAL-BRIDGE", Connection.channelTypes.LOCAL);
+
+// register protocol
+SystemProtocol.init(globalConnection);
+
+Tools.PING(globalConnection, "*");
